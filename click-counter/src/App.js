e3292d0169c2289efb4125fe8b0873141d9b35bb
WittyWatzz <35387099+WittyWatzz@users.noreply.github.com>
2021-03-19 13:19:40 -0400

    React Testing with Jest and Enzyme added decrement counter

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 
 export class App extends Component {
-  state = { counter: 0 };
+  state = { counter: 0, error: '' };
   render() {
     return (
       <div data-test="component-app">
         <h1 data-test="component-counter-display">
           The counter is currently {this.state.counter}
         </h1>
+        <p style={{ color: 'red' }} data-test="component-error-display">
+          {this.state.error}
+        </p>
         <button
           data-test="component-increment-button"
-          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+          onClick={() => {
+            if (this.state.error) {
+              this.setState({ error: '' });
+            }
+            return this.setState({ counter: this.state.counter + 1 });
+          }}
         >
           Increment Counter
+        </button>
+        <button
+          data-test="component-decrement-button"
+          onClick={() => {
+            this.state.counter === 0
+              ? this.setState({ error: 'The counter can not go below zero' })
+              : this.setState({ counter: this.state.counter - 1 });
+          }}
+        >
+          Decrement Counter
         </button>
       </div>
     );
--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -43,6 +43,11 @@ test('renders increment button', () => {
   const buttonComponent = findByTestAttr(wrapper, 'component-increment-button');
   expect(buttonComponent.length).toBe(1);
 });
+test('renders decrement button', () => {
+  const wrapper = setup();
+  const buttonComponent = findByTestAttr(wrapper, 'component-decrement-button');
+  expect(buttonComponent.length).toBe(1);
+});
 test('renders counter display', () => {
   const wrapper = setup();
   const counterDisplayComponent = findByTestAttr(
@@ -51,11 +56,24 @@ test('renders counter display', () => {
   );
   expect(counterDisplayComponent.length).toBe(1);
 });
+test('renders error message', () => {
+  const wrapper = setup();
+  const componentError = findByTestAttr(wrapper, 'component-error-display');
+  expect(componentError.length).toBe(1);
+});
+
 test('renders counter starting at zero', () => {
   const wrapper = setup();
   const initialCounterState = wrapper.state('counter');
   expect(initialCounterState).toBe(0);
 });
+
+test('renders error initial state', () => {
+  const wrapper = setup();
+  const initialErrorState = wrapper.state('error');
+  expect(initialErrorState).toBe('');
+});
+
 test('renders click button increments counter display', () => {
   const counter = 6;
   const wrapper = setup({}, { counter });
@@ -69,6 +87,32 @@ test('renders click button increments counter display', () => {
     'component-counter-display'
   );
   expect(counterDisplayComponent.text()).toContain(counter + 1);
+});
+
+test('renders click button decrements counter', () => {
+  const counter = 6;
+  const wrapper = setup({}, { counter });
+  const button = findByTestAttr(wrapper, 'component-decrement-button');
+  button.simulate('click');
+
+  const decreaseCountDisplay = findByTestAttr(
+    wrapper,
+    'component-counter-display'
+  );
+  expect(decreaseCountDisplay.text()).toContain(counter - 1);
+});
+
+test('renders click button decrements counter below 0', () => {
+  const counter = 0;
+  const wrapper = setup({}, { counter });
+  const button = findByTestAttr(wrapper, 'component-decrement-button');
+  button.simulate('click');
+
+  const decreaseCountDisplay = findByTestAttr(
+    wrapper,
+    'component-error-display'
+  );
+  expect(decreaseCountDisplay.text()).toContain('below zero');
 });
 // test('renders learn react link', () => {
 //   render(<App />);
